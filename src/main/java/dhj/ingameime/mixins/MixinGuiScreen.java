2b91bcd20d97a98c43799105230aea5f368d450e
Comical <dhjcomical@163.com>
2025-07-22 08:48:28 +0800

    [Fix]Can't type text into Minecraft signs

--- src/main/java/dhj/ingameime/mixins/MixinGuiScreen.java
+++ src/main/java/dhj/ingameime/mixins/MixinGuiScreen.java
@@ -4,7 +4,7 @@ import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-import java.io.IOException; // 导入 IOException
+import java.io.IOException;
 
 @Mixin(GuiScreen.class)
 public interface MixinGuiScreen {
